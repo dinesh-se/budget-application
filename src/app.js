bf4d4f2f4aee1ab0ab5f6e3ee815ba07d7a01b2e
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2019-02-03 09:53:51 +0530

    React boilerplate - dev version

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import './style/styles.scss';
+
+ReactDOM.render(<p>This is a react bare bones project to get started.</p>, document.getElementById('app'));
