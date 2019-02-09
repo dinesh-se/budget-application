8e77b61adc0414462eb0ae9df67139b5d5cf178a
Dinesh Haribabu <dinesh.be.gct@gmail.com>
2019-02-10 00:31:32 +0530

    Setup date and currency formatting

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 import { removeExpense } from '../actions/expenses';
 
-export const ExpenseListItem = ( { dispatch, id, description, amount, createdAt } ) => (
+export const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
   <div>
-      <h3>
-        <Link to={`/edit/${id}`}>{description}</Link>
-      </h3>
-      {amount}-{createdAt}&nbsp;
-      <button onClick={() => {
-        dispatch(removeExpense(id));
-      }}>
+    <h3>
+      <Link to={`/edit/${id}`}>{description}</Link>
+    </h3>
+    <p>
+      {numeral(amount / 100).format('$0,0.00')}
+      -
+      {moment(createdAt).format('MMMM Do, YYYY')}
+    </p>
+    <button onClick={() => {
+      dispatch(removeExpense(id));
+    }}>
       Remove</button>
   </div>
 );
